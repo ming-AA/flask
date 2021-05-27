41ecb982bc199623a71597fbea8ce3971f4234bf
Min A <alsdk4556@gmail.com>
2021-05-27 15:54:42 +0900

    질문 등록 기능, CSRF 토큰 기능 추가

--- config.py
+++ config.py
@@ -6,3 +6,6 @@ BASE_DIR = os.path.dirname(__file__)
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 # SQLAlchemy의 이벤트 처리 옵션
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# 환경변수 CSRF 토큰
+SECRET_KEY = "dev"
---
+++ pybo/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired
+
+class QuestionForm(FlaskForm): # FlaskForm 상속
+    subject = StringField('제목', validators=[DataRequired('제목은 필수 입력 항목입니다.')]) # 폼 라벨, 필드값 검증
+    content = TextAreaField('내용', validators=[DataRequired('내용은 필수 입력 항목입니다.')])
