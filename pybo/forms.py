a394f9fd46ce37806273b016cd848270f4bb73e1
Min A <alsdk4556@gmail.com>
2021-05-27 16:17:55 +0900

    답변 등록 기능 수정
    - 폼 추가
    - CSRF 코드, 오류 표시 기능 추가

--- pybo/forms.py
+++ pybo/forms.py
@@ -5,3 +5,6 @@ from wtforms.validators import DataRequired
 class QuestionForm(FlaskForm): # FlaskForm 상속
     subject = StringField('제목', validators=[DataRequired('제목은 필수 입력 항목입니다.')]) # 폼 라벨, 필드값 검증
     content = TextAreaField('내용', validators=[DataRequired('내용은 필수 입력 항목입니다.')])
+
+class AnswerForm(FlaskForm):
+    content = TextAreaField('내용', validators=[DataRequired('내용은 필수 입력 항목입니다.')])
